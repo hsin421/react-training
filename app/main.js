7c796358bf9f945ffe89517af6541365f8cdca43
hsin421 <shinsonh@gmail.com>
2016-09-19 17:11:43 -0400

    add theremin

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import TodoApp from './TodoApp/TodoApp';
+// import { createTheremin } from './lib/Theremin';
 
 class App extends React.Component {
 	render() {
@@ -9,5 +10,55 @@ class App extends React.Component {
 	}
 }
 
+
+// let styles = {}
+
+// styles.theremin = {
+//   height: 200,
+//   width: 200,
+//   fontSize: 10,
+//   border: '1px solid',
+//   cursor: 'crosshair',
+//   margin: 10,
+//   display: 'inline-block'
+// }
+
+// class App extends React.Component {
+
+//   componentWillMount () {
+//     this.theremin = createTheremin()
+//   }
+
+//   play = () => {
+//     this.theremin.play()
+//   }
+
+//   stop = () => {
+//     this.theremin.stop()
+//   }
+
+//   changeTone = (event) => {
+//     let { clientX, clientY } = event
+//     let { top, right, bottom, left } = event.target.getBoundingClientRect()
+//     let pitch = (clientX - left) / 200
+//     let volume = 1 - (clientY - top) / 200
+//     this.theremin.setPitchBend(pitch)
+//     this.theremin.setVolume(volume)
+//   }
+
+//   render () {
+//     return (
+//       <div>
+//         <h1>What does it mean to be declarative?</h1>
+//         <div
+//           style={styles.theremin}
+//           onMouseEnter={this.play}
+//           onMouseLeave={this.stop}
+//           onMouseMove={this.changeTone}
+//         />
+//       </div>
+//     )
+//   }
+// };
 
 render(<App/>, document.getElementById('app'));
